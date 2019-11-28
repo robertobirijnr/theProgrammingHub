910c648401c1c920c81163bca954a64812aaa776
robertobirijnr <robertobirijnr@hotmail.com>
2019-11-28 10:49:57 +0000

    changes

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ mongoose
   .connect(mongoDbConnector, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
   })
   .then(() => console.log('DB connection successful!'));
 
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -13,11 +14,20 @@ const UserSchema = new Schema({
   },
   email: {
     type: String,
-    required: true
+    trim: true,
+    lowercase: true,
+    unique: true,
+    validate: validator.isEmail,
+    required: "Email address is required",
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address"
+    ]
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    minlength: 8
   }
 });
 
